d055571b2f52780dc3fd231c5042873adaf4c5cf
Sivakumar V <sivakumar.bdu@gmail.com>
2015-04-09 05:56:06 +0530

    add value 10 in temp code

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -2,7 +2,8 @@ class Employee < ActiveRecord::Base
   def employee_id
     emp_id
   end
-        def temp_code
-          Random.rand(100)
-        end
+
+  def temp_code
+    Random.rand(100) + 10
+  end
 end
