15fb44f099b7019210d58de8304a50a37e2280c5
Sivakumar V <sivakumar.bdu@gmail.com>
2015-04-09 06:29:13 +0530

    Add extra intentation

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,10 +1,10 @@
 class Employee < ActiveRecord::Base
-  def employee_id
-    emp_id
-  end
+      def employee_id
+        emp_id
+      end
 
-  def temp_code
-    rand = SecureRandom.urlsafe_base64(nil, false)
-    rand + emp_id
-  end
+      def temp_code
+        rand = SecureRandom.urlsafe_base64(nil, false)
+        rand + emp_id
+      end
 end
